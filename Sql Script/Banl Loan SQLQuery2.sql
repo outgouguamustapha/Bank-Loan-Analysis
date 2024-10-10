b56575ede4f3cc8f01940e179ea0aa2fb893a950
OUTGOUGUA MUSTAPHA <139646492+mustapha-py@users.noreply.github.com>
2024-10-11 00:45:39 +0100

    Add files via upload

---
+++ Sql Script/Banl Loan SQLQuery2.sql
@@ -0,0 +1,63 @@
+SELECT *
+FROM bank_loan_data
+
+--Monthly Trends by Issue Date
+SELECT 
+	MONTH(issue_date) AS Month_Number,
+	DATENAME(MONTH, issue_date) AS Month_Name,
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY MONTH(issue_date), DATENAME(MONTH, issue_date)
+ORDER BY MONTH(issue_date)
+
+-- Regional Analysis by State 
+SELECT 
+	address_state,
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY address_state
+ORDER BY COUNT(id) DESC
+
+-- Loan Term Analysis
+SELECT 
+	term,
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY term
+ORDER BY COUNT(id) DESC
+
+-- Employee Length Analysis 
+SELECT 
+	emp_length,
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY emp_length
+ORDER BY COUNT(id) DESC
+
+-- Loan Purpose Breakdown 
+SELECT 
+	purpose,
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY purpose
+ORDER BY COUNT(id) DESC
+
+-- Home Ownership Analysis 
+SELECT 
+	home_ownership,
+	COUNT(id) AS Total_Loan_Applications,
+	SUM(loan_amount) AS Total_Funded_Amount,
+	SUM(total_payment) AS Total_Amount_Received
+FROM bank_loan_data
+GROUP BY home_ownership
+ORDER BY COUNT(id) DESC
